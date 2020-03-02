d48cbe1970633cc68eda0c0ae6ea485874aeae95
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-02 23:26:29 +0300

    emoji config: Add repositoryDataPath

--- config/emoji.php
+++ config/emoji.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'tempFolder'   => 'tempDownload/',
-    'tempFileName' => 'emoji.zip',
-    'githubUrl'    => 'https://github.com/unicode-org/cldr/archive/latest.zip',
+    'tempFolder'         => 'tempDownload/',
+    'tempFileName'       => 'emoji.zip',
+    'githubUrl'          => 'https://github.com/unicode-org/cldr/archive/latest.zip',
+    'repositoryDataPath' => 'cldr-latest/common/annotationsDerived/',
 ];

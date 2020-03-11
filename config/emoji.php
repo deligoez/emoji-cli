134b26c1077bf0267892555b2e5295ced794b460
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-11 08:26:35 +0300

    config/emoji.php: Add second data path

--- config/emoji.php
+++ config/emoji.php
@@ -4,6 +4,7 @@ return [
     'tempFolder'         => 'tempDownload/',
     'tempFileName'       => 'emoji.zip',
     'githubUrl'          => 'https://github.com/unicode-org/cldr/archive/latest.zip',
-    'repositoryDataPath' => 'cldr-latest/common/annotationsDerived/',
+    'repositoryDataPath' => 'cldr-latest/common/annotations/',
+    'repositoryDataPath2' => 'cldr-latest/common/annotationsDerived/',
     'dataFolderName'     => 'data/',
 ];
